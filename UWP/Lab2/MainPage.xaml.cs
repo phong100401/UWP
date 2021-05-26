99a0d091afd977f67c79330f1f24aa725922499c
phong100401 <65002403+phong100401@users.noreply.github.com>
2021-05-26 17:53:55 +0700

    lab2
    lab2

---
+++ UWP/Lab2/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Lab2.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Lab2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string email = inputEmail.Text;
+            string title = inputTitle.Text;
+            string content = inputContent.Text;
+
+            TxtBlockEmail.Text = "Đã gửi đến: " + email;
+            TxtBlockSubject.Text = "Chủ đề thư: " + title;
+            TxtBlockContent.Text = "Nội dung thư:\n" + content;
+
+            User u1 = new User(email,title, content);
+            ListUsers.Items.Add(u1);
+        }
+        private void AppbarButton_Click(object sender, RoutedEventArgs e)
+        {
+            SV.IsPaneOpen = !SV.IsPaneOpen;
+        }
+
+        private void ListUsers_Loaded(object sender, RoutedEventArgs e)
+        {
+            //User u1 = new User("abc@gmail.com", "abc", "dasdasdasd");
+            //User u2 = new User("acs@gmail.com", "adc", "aqeasdasd");
+            //ListUsers.Items.Add(u1);
+            //ListUsers.Items.Add(u2);
+        }
+    }
+}
---
+++ UWP/Lab2/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Models
+{
+    class User
+    {
+        private string email;
+        private string title;
+        private string content;
+
+        public User(string email, string title, string content)
+        {
+            Email = email;
+            Title = title;
+            Content = content;
+        }
+
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+        public string Content
+        {
+            get => content;
+            set => content = value;
+        }
+    }
+}
